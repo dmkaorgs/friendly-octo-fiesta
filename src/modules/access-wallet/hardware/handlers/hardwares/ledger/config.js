813c79fec39cf4c1d7bb9251a63a8b5e1b906657
Vnza Vnza <123856347+Vnza69@users.noreply.github.com>
2023-04-10 19:07:07 +0700

    Initial commit

---
+++ src/modules/access-wallet/hardware/handlers/hardwares/ledger/config.js
@@ -0,0 +1,7 @@
+export const appNames = {
+  ETH: 'Ethereum',
+  ETC: 'Ethereum Classic',
+  GO: 'GoChain',
+  XDC: 'XDC Network',
+  Rootstock: 'RSK'
+};
---
+++ src/utils/networks/types/index.js
@@ -0,0 +1,25 @@
+import ETC from './ETC.js';
+import ETH from './ETH.js';
+import GOERLI from './GOERLI.js';
+import BSC from './BSC.js';
+import MATIC from './MATIC.js';
+import GO from './GO.js';
+import MINTME from './MINTME';
+import XDC from './XDC';
+import MOONBEAM from './MOONBEAM';
+import MOONRIVER from './MOONRIVER';
+import ROOTSTOCK from './ROOTSTOCK';
+
+export {
+  ETH,
+  GOERLI,
+  ETC,
+  BSC,
+  MATIC,
+  GO,
+  MINTME,
+  XDC,
+  MOONBEAM,
+  MOONRIVER,
+  ROOTSTOCK
+};
